0f28777a991f2221c1ad2dce9dcd6e8d3aa52c1a
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-09-29 18:07:02 +0200

    Terminado ejerciciio 1 Tema 1

--- ConceptosBasicos/src/Java1/Exercise1.java
+++ ConceptosBasicos/src/Java1/Exercise1.java
@@ -1,6 +1,9 @@
 package Java1;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Exercise1 {
 	
@@ -163,12 +166,70 @@ public class Exercise1 {
 		int suma = dia + mes + año;
 		int num1 = suma % 10;
 		int num2 = (suma / 10) % 10;
-		int num3 = (suma / 10) % 10;
-		//int num4 = num /10;
-		
-		//System.out.println("Tú número de la suerte es: " + ());
+		int num3 = (suma / 100) % 10;
+		int num4 = suma / 1000;
+		int numSuerte = num1 + num2 + num3 + num4;
+		System.out.println("Tu número de la suerte es: " + numSuerte);
 	}
-	public static void main(String[] args) {
+	/*14. Pregunta al usuario su edad e infórmale de cuantos años tendrá dentro de 15 años.*/
+	public static void exercise14() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime tu edad: ");
+		int edad = sc.nextInt();
+		System.out.println("Dentro de 15 años tendras: "+(edad+15));
+	}
+	/*15. Pregunta al usuario el nombre de 3 productos con sus respectivos precios. Debes mostrar dichos datos
+		formateados por la consola. El nombre tendrá 15 caracteres, el precio 12 (con 2 decimales) y
+		finalmente, muestra el precio con IVA (21%), también con 12 caracteres y 2 decimales. El nombre
+		debe estar alineado a la izquierda (espacios a la derecha) y los precios a la derecha.*/
+	public static void exercise15() throws IOException {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("Producto uno: ");
+		String produc1 = reader.readLine();
+		System.out.print("Precio: ");
+		double precio1 =Double.parseDouble(reader.readLine());
+		
+		System.out.print("Producto dos: ");
+		String produc2 =  reader.readLine();
+		System.out.print("Precio: ");
+		double precio2 = Double.parseDouble(reader.readLine());
+		
+		System.out.print("Producto tres: ");
+		String produc3 =  reader.readLine();
+		System.out.print("Precio: ");
+		double precio3 = Double.parseDouble(reader.readLine());
+		
+		System.out.printf("%-15s%-12s%12s\n", "NOMBRE", "PRECIO", "CON IVA");
+		System.out.printf("%-15s%11.2f€%11.2f€\n", produc1, precio1, ((precio1*0.21)+precio1));
+		System.out.printf("%-15s%11.2f€%11.2f€\n", produc2,  precio2, ((precio2*0.21)+precio2));
+		System.out.printf("%-15s%11.2f€%11.2f€\n", produc3,  precio3, ((precio3*0.21)+precio3));
+	}
+	/*20. Dile al usuario que escriba 3 dígitos numéricos (no los muestres por la consola). A continuación suma
+		los números e informa al usuario de su resultado:*/
+	public static void exercise20() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime 3 dígitos enteros: ");
+		double num1 = sc.nextDouble();
+		double num2 = sc.nextDouble();
+		double num3 = sc.nextDouble();
+		System.out.println("Has escrito: "+num1+" + "+num2+" + "+num3+" = "+(num1+num2+num3));
+	}
+	
+	/*21. Pregúntale al usuario su nombre y edad. A continuación muestra la salida con este formato usando
+		una única instrucción System.out.println. El espacio de la izquierda del nombre y edad es un
+		tabulado.*/
+	public static void exercise21() {
+		System.out.println("Dime tu nombre: ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		System.out.println("Dime tu edad: ");
+		int age = sc.nextInt();
+		System.out.println("Estos son tus datos\n\tnombre: "+name+"\n\tedad: "+age); //\t tabula
+	}
+	
+	public static void main(String[] args) throws IOException {
 		
 		/*exercise1();
 		exercise2();
@@ -181,7 +242,11 @@ public class Exercise1 {
 		exercise9();
 		exercise10();
 		exercise11();
-		exercise12();*/
+		exercise12();
 		exercise13();
+		exercise14();
+		exercise15();
+		exercise20();*/
+		exercise21();
 	}
 }
