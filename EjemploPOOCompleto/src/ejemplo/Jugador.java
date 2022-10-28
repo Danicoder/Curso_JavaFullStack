5642a001483d3b85434cf175d381c7b0bf89f09a
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-10-28 22:08:33 +0200

    eliminación constructor vacío

--- EjemploPOOCompleto/src/ejemplo/Jugador.java
+++ EjemploPOOCompleto/src/ejemplo/Jugador.java
@@ -6,22 +6,16 @@ public class Jugador {
     private int edad;
     private double sueldo;
 
-    public Jugador() {
+     public Jugador(String nombre, int edad, double sueldo) {
         this.nombre = nombre;
-        setEdad(edad);
-        setSalario(salario);
+        this.edad = edad; // o setEdad(edad);
+        this.sueldo = sueldo;
     }
     //constructor de copia
     public Jugador(Jugador j){
         this.nombre = j.nombre;
         this.edad = j.edad;
         this.sueldo = j.sueldo;
-    }
-
-    public Jugador(String nombre, int edad, double sueldo) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.sueldo = sueldo;
     }
     public String getNombre() {
         return nombre;
