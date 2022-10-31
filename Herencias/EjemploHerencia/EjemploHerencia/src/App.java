2c539e3684c0218ed30b3c48e534565fc53670f9
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-10-31 17:21:11 +0100

    Ejercicios de Herencias

---
+++ Herencias/EjemploHerencia/EjemploHerencia/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        TiendaLicores tienda  = new TiendaLicores();
+        tienda.bienvenida();
+    }
+}
---
+++ Herencias/EjemploHerencia/EjemploHerencia/src/Tienda.java
@@ -0,0 +1,6 @@
+public class Tienda {
+
+    public void bienvenida() {
+        System.out.println("“¡Bienvenido a nuestra tienda!");
+    }
+}
