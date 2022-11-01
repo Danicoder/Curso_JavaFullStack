305ad198f21bcd8ed0cf32bd3815d8897c65b2f1
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-11-01 19:08:14 +0100

    polimorfismo

--- Herencias/ClasesAbstractas/EjerciciosAbstract/src/App.java
+++ Herencias/ClasesAbstractas/EjerciciosAbstract/src/App.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
-    public static void main(String[] args){
+    public static void ejemploAbstracto() {
         Circulo ci = new Circulo(4);
         Cuadrado cu = new Cuadrado(6);
 
@@ -10,5 +14,24 @@ public class App {
         System.out.println("Cuadrado (lado: )"+cu.getLado());
         System.out.println("Área: "+cu.getArea());
         System.out.println("Perímetro: "+cu.getPerimetro());
+    }
+    public static void ejemploPolimorfismo() {
+
+        List<Figura> Figuras = new ArrayList<>(Arrays.asList(new Circulo(7.89),new Circulo(4.08),new Circulo(10),
+                                                            new Cuadrado(6.5),new Cuadrado(8),new Cuadrado(7)));
+
+        for (Figura figura : Figuras) {
+            if(figura instanceof Cuadrado){
+                System.out.println("*** Círculo - Radio: " + ((Cuadrado)figura).getLado() + "***");
+            }
+            else{
+                System.out.println("*** Cuadrado - Lado: " + ((Circulo)figura).getRadio() + "***");
+            }
+            System.out.printf("Area: %.2f. Perímetro: %.2f\n",figura.getArea(),figura.getPerimetro());
+        }
+    }
+    public static void main(String[] args){
+        ejemploAbstracto();
+        ejemploPolimorfismo();
     }
 }
