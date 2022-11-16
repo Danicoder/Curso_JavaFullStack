ea2e47afba8895c20951652373570db5c544b2e4
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-11-16 17:24:46 +0100

    linéas que van en la otra tabla

--- Spring/puntuacion/src/main/java/compuntuacion/puntuacion/puntuaciones/PuntuacionRepository.java
+++ Spring/puntuacion/src/main/java/compuntuacion/puntuacion/puntuaciones/PuntuacionRepository.java
@@ -8,7 +8,4 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository                                                    //le paso la cp y sú tipo
-public interface PuntuacionRepository extends CrudRepository<Puntuacion,Integer> {
-    @Query("SELECT * FROM category WHERE name LIKE %:nombre%")
-    List<Category> buscarNombre(String nombre);
-}
+public interface PuntuacionRepository extends CrudRepository<Puntuacion,Integer> {}
