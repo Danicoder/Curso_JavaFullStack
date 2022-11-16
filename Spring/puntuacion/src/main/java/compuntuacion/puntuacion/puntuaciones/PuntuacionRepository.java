564369401dc34c3099a3a04491955b778a8c52c5
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-11-16 22:09:20 +0100

    coneción bbdd

--- Spring/puntuacion/src/main/java/compuntuacion/puntuacion/puntuaciones/PuntuacionRepository.java
+++ Spring/puntuacion/src/main/java/compuntuacion/puntuacion/puntuaciones/PuntuacionRepository.java
@@ -1,9 +1,5 @@
 package compuntuacion.puntuacion.puntuaciones;
 
-import java.util.List;
-import java.util.Locale.Category;
-
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
