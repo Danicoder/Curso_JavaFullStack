5968f1d58e21ea05af68ed7b6cb1edefe5978ce9
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-11-16 17:00:27 +0100

    correción devolución de errores

--- Spring/puntuacion/src/main/java/compuntuacion/puntuacion/puntuaciones/PuntuacionRepository.java
+++ Spring/puntuacion/src/main/java/compuntuacion/puntuacion/puntuaciones/PuntuacionRepository.java
@@ -1,5 +1,14 @@
 package compuntuacion.puntuacion.puntuaciones;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import java.util.Locale.Category;
 
-public interface PuntuacionRepository extends CrudRepository<Puntuacion,Integer> {}
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository                                                    //le paso la cp y sú tipo
+public interface PuntuacionRepository extends CrudRepository<Puntuacion,Integer> {
+    @Query("SELECT * FROM category WHERE name LIKE %:nombre%")
+    List<Category> buscarNombre(String nombre);
+}
