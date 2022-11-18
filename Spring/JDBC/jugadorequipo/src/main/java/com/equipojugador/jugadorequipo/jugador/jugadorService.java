463c6672ce65c250b4fcf75b3186dcb240eb4692
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-11-18 23:48:47 +0100

    archivo sin guardar

--- Spring/JDBC/jugadorequipo/src/main/java/com/equipojugador/jugadorequipo/jugador/jugadorService.java
+++ Spring/JDBC/jugadorequipo/src/main/java/com/equipojugador/jugadorequipo/jugador/jugadorService.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.equipojugador.jugadorequipo.jugadorRepository;
-
 import lombok.RequiredArgsConstructor;
 
 @Service //gestiona la lógica de negocio
