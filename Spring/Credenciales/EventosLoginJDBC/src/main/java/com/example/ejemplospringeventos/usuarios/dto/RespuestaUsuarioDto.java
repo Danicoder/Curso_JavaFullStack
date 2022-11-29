472a3a46a2c7d5608788e3336891074e1ad792cf
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-11-29 14:28:38 +0100

    asignando nuevo campo

---
+++ Spring/Credenciales/EventosLoginJDBC/src/main/java/com/example/ejemplospringeventos/usuarios/dto/RespuestaUsuarioDto.java
@@ -0,0 +1,9 @@
+package com.example.ejemplospringeventos.usuarios.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class RespuestaUsuarioDto {
+    UsuarioConEventosDto usuario;
+}
