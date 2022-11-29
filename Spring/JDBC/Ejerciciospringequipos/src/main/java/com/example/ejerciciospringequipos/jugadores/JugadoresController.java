0969c12621bdb862e26739ef035ad2a42fe4be15
Danicoder <56139050+Danicoder@users.noreply.github.com>
2022-11-29 17:40:40 +0100

    archivos

--- Spring/JDBC/Ejerciciospringequipos/src/main/java/com/example/ejerciciospringequipos/jugadores/JugadoresController.java
+++ Spring/JDBC/Ejerciciospringequipos/src/main/java/com/example/ejerciciospringequipos/jugadores/JugadoresController.java
@@ -2,12 +2,15 @@ package com.example.ejerciciospringequipos.jugadores;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -46,4 +49,9 @@ public class JugadoresController {
     return jugadoresService.insert(jugador);
   }
 
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(int id) {
+    jugadoresService.delete(id);
+  }
 }
